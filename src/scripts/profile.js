b4bee8cd0bca6076cb5923018d7a56e69f570593
Matheus <mabatista_10@hotmail.com>
2023-08-29 10:06:00 -0300

    Correcoes conforme rubrica

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -16,7 +16,7 @@ const renderUserInfo = (currentUser) => {
 const button = document.querySelector("button")
 button.addEventListener("click", () => {
     localStorage.clear()
-    location.replace("http://127.0.0.1:5500")
+    location.replace("/index.html")
 })
 
 const renderUserRepos = async () => {
